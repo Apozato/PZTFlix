977c56f7ce8cfa04d2b519966fc4d53ef4d68e59
Ana Laura Pozato <apozato@gmail.com>
2025-04-09 22:55:18 -0300

    site

---
+++ js/login.js
@@ -0,0 +1,6 @@
+// Ao carregar a página executada as funções de buscar os dados4
+document.addEventListener("DOMContentLoaded", async () => {
+    setTimeout(() => {
+        toggleLoading();
+    }, 2000);
+});
